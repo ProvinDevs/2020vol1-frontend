3dcfdacfc016ff10516f68b0af0e240952d4ece5
shun-shobon <shun.shobon@gmail.com>
2020-08-18 03:06:18 +0900

    feat: add sample files

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import AboutPage from "./pages/About";
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <div>
+      <Link to="/">HOME</Link>|
+      <Link to="/about">ABOUT</Link>
+    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About: React.FC = () => (
+  <h1>This is About</h1>
+);
+
+export default About;
