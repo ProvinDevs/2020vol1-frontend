a24f1b8e5d3e7c9889c9dc83e5565b8bbfa05c3f
shun-shobon <shun.shobon@gmail.com>
2020-08-18 14:44:54 +0900

    build(typescript): add image types

---
+++ src/global.d.ts
@@ -0,0 +1,12 @@
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
