2b4a097a65f283fb41933bc69ff65209e71aa54f
unios103 <unios103@gmail.com>
2020-08-19 21:04:23 +0900

    setup material ui (#24)
    * material uiを追加
    
    * material uiを追加
    
    * FC

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { FC } from "react";
 
 import styles from "../scss/pages/about.scss";
 
-const About: React.FC = () => <h1 className={styles.about}>This is About</h1>;
+const About: FC = () => <h1 className={styles.about}>This is About</h1>;
 
 export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import React from "react";
+import React, { FC } from "react";
 
-const Home: React.FC = () => <h1>This is Home</h1>;
+const Home: FC = () => <h1>This is Home</h1>;
 
 export default Home;
