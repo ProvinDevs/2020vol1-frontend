50932a60835ead00aaaff093600554ba5a97d4e8
unios103 <unios103@gmail.com>
2020-08-19 19:10:54 +0900

    material uiを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { FC } from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core";
 
 import HomePage from "./pages/Home";
 import AboutPage from "./pages/About";
+import { theme } from "./theme";
 
-const App: React.FC = () => (
+const App: FC = () => (
   <BrowserRouter>
-    <div>
-      <Link to="/">HOME</Link> | <Link to="/about">ABOUT</Link>
-    </div>
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Link to="/">HOME</Link> | <Link to="/about">ABOUT</Link>
+      </div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+      </Switch>
+    </ThemeProvider>
   </BrowserRouter>
 );
 
