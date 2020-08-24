eb6ae19da1b8d2c28bd51643e156bcaff20db07a
isso0424 <isso04240129@gmail.com>
2020-08-24 22:19:36 +0900

    [Add]Create ClassDetailPage.tsx

---
+++ src/pages/ClassDetailPage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+import Header, { HeaderProps } from "../components/common/Header";
+import PageContainer from "../components/common/Container";
+
+const headerProps: HeaderProps = {
+  role: "生徒",
+  href: "/about",
+};
