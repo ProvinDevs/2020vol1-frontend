b926358ed1ce601bb2843bb8cd4da529474da30d
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-08-24 21:40:23 +0900

    fix babel configuration for async/await

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/Home";
@@ -7,19 +7,25 @@ import TeacherHomePage from "./pages/TeacherHome";
 import Footer from "./components/common/Footer";
 import PageWrapper from "./theme";
 
+import { SampleApiClient } from "./api/impls/sample";
+
 import "./scss/global.scss";
 
-const App: FC = () => (
-  <BrowserRouter>
-    <PageWrapper>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route exact path="/teacher" component={TeacherHomePage} />
-      </Switch>
-      <Footer />
-    </PageWrapper>
-  </BrowserRouter>
-);
+const App: FC = () => {
+  const apiClient = useState(new SampleApiClient())[0];
+
+  return (
+    <BrowserRouter>
+      <PageWrapper>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route exact path="/teacher" component={TeacherHomePage} />
+        </Switch>
+        <Footer />
+      </PageWrapper>
+    </BrowserRouter>
+  );
+};
 
 export default App;
