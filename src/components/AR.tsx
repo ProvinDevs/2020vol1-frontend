a374955ef37ddaecafc1659d105242923ca9128e
shun-shobon <shun.shobon@gmail.com>
2020-08-24 17:58:40 +0900

    feat(ar): add props

--- src/components/AR.tsx
+++ src/components/AR.tsx
@@ -1,8 +1,14 @@
 import React, { FC, useEffect } from "react";
+import { File } from "../api";
 
 import styles from "../scss/components/ar.scss";
 
-const AR: FC = () => {
+type Props = {
+  files: Array<File>;
+};
+
+// TODO: 現在未使用引数。後で使用すること。
+const AR: FC<Props> = ({ files }) => {
   let renderer: THREE.WebGLRenderer;
   let scene: THREE.Scene;
   let camera: THREE.Camera;
