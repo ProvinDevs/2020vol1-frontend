b9f823a4cda6d79a5a0d9dae44308e0af681a902
Shuntaro Nishizawa <shun.shobon@gmail.com>
2020-08-26 22:42:08 +0900

    ARマーカーの追加 (#60)

--- src/global.d.ts
+++ src/global.d.ts
@@ -12,6 +12,10 @@ declare module "*.gif" {
   const src: string;
   export default src;
 }
+declare module "*.patt" {
+  const src: string;
+  export default src;
+}
 
 declare module "*.scss" {
   const classes: { readonly [key: string]: string };
