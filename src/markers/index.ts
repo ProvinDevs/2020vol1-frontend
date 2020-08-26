4bab611fba6f10ee35fbdfd4994e35d2568f5612
shun-shobon <shun.shobon@gmail.com>
2020-08-26 21:54:56 +0900

    feat(ar): add get marker image, pattern url

--- src/markers/index.ts
+++ src/markers/index.ts
@@ -80,6 +80,14 @@ const markerList: Array<MarkerData> = [
   },
 ];
 
+export const getMarkerImageUrl = (file: File): string | undefined => {
+  return markerList.find((markerData) => markerData.id === file.markerID)?.imageSrc;
+};
+
+export const getMarkerPatternUrl = (file: File): string | undefined => {
+  return markerList.find((markerData) => markerData.id === file.markerID)?.patternSrc;
+};
+
 // TODO: マーカーの画像が決まったらきちんとした実装を書きます。それまではAR.js標準のマーカー画像を返します。
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getMarker = (file: File): string | undefined => {
