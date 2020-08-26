77d732168016ddb21df6f5958f95276306247783
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-08-26 23:16:12 +0900

    master ->

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/Home";
@@ -14,26 +14,29 @@ import { SampleApiClient } from "./api/impls/sample";
 
 import "./scss/global.scss";
 
-const App: FC = () => {
-  const [api, _] = useState(new SampleApiClient());
-  return (
-    <BrowserRouter>
-      <PageWrapper>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route exact path="/teacher" component={TeacherHomePage} />
-          <Route path="/join" render={() => <JoinClass api={api} />} />
-          <Route exact path="/teacher/class/:id" component={() => <ClassEditPage client={api} />} />
-          <Route
-            path="/student/class/:passphrase"
-            component={() => <ClassPage apiClient={api} />}
-          />
-        </Switch>
-        <Footer />
-      </PageWrapper>
-    </BrowserRouter>
-  );
-};
+const apiClient = new SampleApiClient();
+
+const App: FC = () => (
+  <BrowserRouter>
+    <PageWrapper>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/teacher" component={TeacherHomePage} />
+        <Route exact path="/join" component={() => <JoinClass api={apiClient} />} />
+        <Route
+          exact
+          path="/teacher/class/:id"
+          component={() => <ClassEditPage client={apiClient} />}
+        />
+        <Route
+          path="/student/class/:passphrase"
+          component={() => <ClassPage apiClient={apiClient} />}
+        />
+      </Switch>
+      <Footer />
+    </PageWrapper>
+  </BrowserRouter>
+);
 
 export default App;
