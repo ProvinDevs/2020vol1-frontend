952c7fdb67f9705ddf8cddd8a1f0f39d6d35224a
shun-shobon <shun.shobon@gmail.com>
2020-08-26 22:01:09 +0900

    feat(ar): change getMarker return type

--- src/markers/index.ts
+++ src/markers/index.ts
@@ -88,8 +88,6 @@ export const getMarkerPatternUrl = (file: File): string | undefined => {
   return markerList.find((markerData) => markerData.id === file.markerID)?.patternSrc;
 };
 
-// TODO: マーカーの画像が決まったらきちんとした実装を書きます。それまではAR.js標準のマーカー画像を返します。
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getMarker = (file: File): string | undefined => {
-  return THREEx.ArToolkitContext.baseURL + "../data/data/patt.hiro";
+export const getMarker = (file: File): MarkerData | undefined => {
+  return markerList.find((markerData) => markerData.id === file.markerID);
 };
