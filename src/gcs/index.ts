60670a36c65ed99819daa4edab7f6ef606781d2f
shun-shobon <shun.shobon@gmail.com>
2020-08-27 14:37:30 +0900

    fix(gcs): change gcs to firebase

--- src/gcs/index.ts
+++ src/gcs/index.ts
@@ -1,34 +1,15 @@
-import { Storage, Bucket } from "@google-cloud/storage";
-import { File } from "../api";
+import * as firebase from "firebase/app";
+import "firebase/storage";
 import token from "./token.json";
 
-// ***†卍 ハードコーディング 卍†***
-const BUCKET_NAME = "provindevs-2020-files";
+const FILE_REF_NAME = "provindevs-2020-files";
 
 class GCS {
-  private bucket: Bucket;
+  private fileRef: firebase.storage.Reference;
 
-  constructor(private bucketName: string = BUCKET_NAME) {
-    this.bucket = new Storage({ credentials: token }).bucket(bucketName);
-  }
-
-  public async addNewFile(file: File, fileSrc: string): Promise<void> {
-    await this.bucket.upload(fileSrc, {
-      destination: file.id,
-      gzip: true,
-    });
-    await this.bucket.file(file.id).makePublic();
-  }
-
-  public async deleteFile(file: File): Promise<File> {
-    await this.bucket.file(file.id).delete();
-    return file;
-  }
-
-  public async getFileUrl(file: File): Promise<string | undefined> {
-    if (await this.bucket.file(file.id).exists()) {
-      return `https://storage.googleapis.com/${this.bucketName}/${file.id}`;
-    }
+  constructor(fileRefName: string = FILE_REF_NAME) {
+    firebase.initializeApp(token);
+    this.fileRef = firebase.storage().ref(fileRefName);
   }
 }
 
