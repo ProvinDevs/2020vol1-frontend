952a3cb5a0b7b583f4bae76ad6fdf01d549e18e8
UniokaWorld <uniokaworld@gmail.com>
2020-08-29 03:34:44 +0900

    画像がでるようにした

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,7 @@ import {
   CardContent,
   Typography,
   Button,
+  CardMedia,
 } from "@material-ui/core";
 
 import Header, { HeaderProps } from "../components/common/Header";
@@ -15,11 +16,17 @@ import PageContainer from "../components/common/Container";
 
 import styles from "../scss/pages/home.scss";
 
+import SumahoImage from "../images/sumaho_image.png";
+import SenseiImage from "../images/sensei_image.png";
+import SeitoImage from "../images/seito_image.png";
+import DownladImage from "../images/download_image.png";
+
 type menuContentsType = {
   title: string;
   contents: string;
   linkText: string;
   link: string;
+  image: string;
 };
 
 const headerProps: HeaderProps = {
@@ -34,29 +41,34 @@ const menuContents: menuContentsType[] = [
       "ARのマーカーを黒板に貼り付けて、カメラを通して見ることで資料を映しだすことができます。",
     linkText: "詳しく見る",
     link: "/about",
+    image: SumahoImage,
   },
   {
     title: "教師の方",
     contents: "授業を作成しましょう。",
     linkText: "始める",
     link: "/teacher",
+    image: SenseiImage,
   },
   {
     title: "生徒の方",
     contents: "授業コードを入力して始めましょう。",
     linkText: "始める",
     link: "/student/join",
+    image: SeitoImage,
   },
   {
     title: "マーカーの入手",
     contents: "ここから必要なマーカーをダウンロードできます。",
     linkText: "見に行く",
     link: "/download",
+    image: DownladImage,
   },
 ];
 
 const HomeCard: FC<menuContentsType> = (menu) => (
   <Card className={styles.homeMenu}>
+    <CardMedia component="img" image={menu.image} title={`${menu.title}のイメージ`} />
     <CardContent>
       <Typography gutterBottom variant="h5" component="h2" className={styles.homeTitle}>
         {menu.title}
