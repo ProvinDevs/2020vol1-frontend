a72c16477a527d80bcbb2a47ba8264a6a004f74e
unios103 <unios103@gmail.com>
2020-08-29 00:15:45 +0900

    colsの数を画面サイズによって変更

--- src/pages/MarkerDownload.tsx
+++ src/pages/MarkerDownload.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, GridList, GridListTile, GridListTileBar, IconButton } from "@material-ui/core";
+import { GetApp } from "@material-ui/icons";
+import EventListener from "react-event-listener";
 
 import { markerList } from "../markers/index";
 import Header, { HeaderProps } from "../components/common/Header";
 import PageContainer from "../components/common/Container";
-import { Container, GridList, GridListTile, GridListTileBar, IconButton } from "@material-ui/core";
-import { GetApp } from "@material-ui/icons";
 
 const headerProps: HeaderProps = {
   buttonText: "About",
@@ -12,6 +13,17 @@ const headerProps: HeaderProps = {
 };
 
 const MarkerDownloadPage = (): JSX.Element => {
+  const [windowSize, setWindowState] = useState(document.documentElement.clientWidth < 600 ? 1 : 3);
+
+  const cols = () => {
+    const window = document.documentElement.clientWidth;
+    if (window < 600) {
+      setWindowState(1);
+    } else {
+      setWindowState(3);
+    }
+  };
+
   const clickEventHandler = (srcURL: string, imageID: string) => {
     const a = document.createElement("a");
     a.href = srcURL;
@@ -21,11 +33,12 @@ const MarkerDownloadPage = (): JSX.Element => {
 
   return (
     <>
+      <EventListener target="window" onResize={cols} />
       <Header {...headerProps} />
       <PageContainer>
         <h1>マーカーのダウンロード</h1>
         <Container maxWidth="md">
-          <GridList cellHeight={300} cols={3}>
+          <GridList cellHeight={300} cols={windowSize}>
             {markerList.map((marker, index) => (
               <GridListTile key={index} cols={1}>
                 <img id={marker.id} src={marker.imageSrc} alt={marker.id} />
